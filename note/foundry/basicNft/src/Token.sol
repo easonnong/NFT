4861c869e41fd9decb1edcf482256122b7e851c0
easonnong <nongeason@gmail.com>
2023-01-10 17:03:52 +0800

    override and metadata

--- note/foundry/basicNft/src/Token.sol
+++ note/foundry/basicNft/src/Token.sol
@@ -7,7 +7,25 @@ import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {ERC721A} from "erc721a/contracts/ERC721A.sol";
 
 contract Token is Ownable, OperatorFilterer, ERC2981, ERC721A {
-    constructor() ERC721A("Token", "TOKEN") {}
+    enum SaleStates {
+        CLOSED,
+        ALLOWLIST,
+        PUBLIC
+    }
+
+    uint256 public constant MAX_SUPPLY = 100;
+    uint256 public constant MAX_PER_MINT = 3;
+
+    SaleStates public SaleState;
+    address public royaltyReceiver;
+    bool public operatorFilteringEnabled = true;
+    string public _baseTokenURI = "";
+
+    constructor() ERC721A("Token", "TOKEN") {
+        _registerForOperatorFiltering();
+        // Set initial 5% royalty
+        // _setDefaultRoyalty(royaltyReceiver, 500);
+    }
 
     // =============================================================
     //                           ERC165
@@ -18,5 +36,38 @@ contract Token is Ownable, OperatorFilterer, ERC2981, ERC721A {
         return
             ERC721A.supportsInterface(interfaceId) ||
             ERC2981.supportsInterface(interfaceId);
+    }
+
+    // =============================================================
+    //                           ERC2981
+    // =============================================================
+    function setDefaultRoyalty(
+        address receiver,
+        uint96 feeNumerator
+    ) public onlyOwner {
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+
+    // =============================================================
+    //                           METADATA
+    // =============================================================
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length != 0
+                ? string(abi.encodePacked(baseURI, _toString(tokenId), ".json"))
+                : "";
     }
 }
