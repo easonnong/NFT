1b681d0ffaaf7bd1b17e39ed7f6d8a0beca8920d
easonnong <nongeason@gmail.com>
2023-01-10 15:56:24 +0800

    erc165

--- note/foundry/basicNft/src/Token.sol
+++ note/foundry/basicNft/src/Token.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Token {
-    constructor() {}
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OperatorFilterer} from "closedsea/src/OperatorFilterer.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
+import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+
+contract Token is Ownable, OperatorFilterer, ERC2981, ERC721A {
+    constructor() ERC721A("Token", "TOKEN") {}
+
+    // =============================================================
+    //                           ERC165
+    // =============================================================
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721A, ERC2981) returns (bool) {
+        return
+            ERC721A.supportsInterface(interfaceId) ||
+            ERC2981.supportsInterface(interfaceId);
+    }
 }
